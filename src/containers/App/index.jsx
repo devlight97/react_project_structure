8075a112113fe1c6e94d526ae2dd6063bb1bd875
devlight97 <dev.light97@gmail.com>
2019-04-22 03:08:22 +0700

    The first commit for project

---
+++ src/containers/App/index.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import TestButton from '@containers/TestButton';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { makeGetClassification } from './selector';
+import {
+  Title,
+} from './App.styled';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mark: 0,
+      conductMark: 0,
+    };
+  }
+
+  getTextInput(evt) {
+    const { name, value } = evt.target;
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    return (
+      <div>
+        <Title>Hello App</Title>
+        <span>mark</span>
+        <input
+          onChange={evt => this.getTextInput(evt)}
+          type="text"
+          name="mark"
+          pattern="[0-9]*"
+        />
+        <br />
+        <br />
+        <span>conductMark</span>
+        <input
+          onChange={evt => this.getTextInput(evt)}
+          type="text"
+          name="conductMark"
+          pattern="[0-9]*"
+        />
+        <TestButton mark={this.state.mark} conductMark={this.state.conductMark} />
+        <h3>Result: {this.props.classification}</h3>
+        <Link to="/app2">router test app 2</Link>
+      </div>
+    );
+  }
+}
+
+const makeMapStateToProps = () => {
+  const getClassification = makeGetClassification();
+  const mapStateToProps = (state, props) => {
+    return {
+      classification: getClassification(state, props),
+    };
+  };
+  return mapStateToProps;
+};
+const mapStateToProps = makeMapStateToProps();
+
+export default connect(mapStateToProps)(App);
---
+++ src/root/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import AppRouter from './appRouter';
+
+const RootApp = () => <AppRouter />;
+export default RootApp;
